d82607c9d4b073507bb0063c774a7006efddd967
Shadhil24 <shadhil@imcapsule.io>
2024-03-17 13:19:11 +0530

    data validation code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.textSummarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
-# from src.textSummarizer.pipeline.data_validation import DataValidationTrainingPipeline
+from src.textSummarizer.pipeline.data_validation import DataValidationTrainingPipeline
 # from src.textSummarizer.pipeline.data_transformation import DataTransformationTrainingPipeline
 # from src.textSummarizer.pipeline.model_trainer import ModelTrainerTrainingPipeline
 # from src.textSummarizer.pipeline.model_evaluation import ModelEvaluationTrainingPipeline
@@ -12,6 +12,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
